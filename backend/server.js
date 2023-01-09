dba30a3995137a8f6cfcd256283ecec1a5f952cb
Árpád <developedbybarley@gmail.com>
2023-01-09 12:24:26 +0100

    Initial frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5050;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import {useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    axios.get('http://localhost:8080/')
+    .then(res => console.log(res.data))
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
